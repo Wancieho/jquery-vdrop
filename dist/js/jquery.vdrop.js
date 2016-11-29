7dd22871d4f39f04201d2d888d28829ad463c94b
Sandy Lin <sandy900201@gmail.com>
2016-11-29 12:53:47 +0200

    Update code structure (Alt + Shift + F).

--- dist/js/jquery.vdrop.js
+++ dist/js/jquery.vdrop.js
@@ -9,126 +9,127 @@
  */
 ;
 (function ($, window, document, undefined) {
-	'use strict';
+    'use strict';
 
-	var pluginName = 'vDrop';
-	var defaults = {
-		transitionSpeed: 150,
-		allowMultiple: true,
-		theme: ''
-	};
-	var selects = [];
+    var pluginName = 'vDrop';
+    var defaults = {
+        transitionSpeed: 150,
+        allowMultiple: true,
+        theme: ''
+    };
+    var selects = [];
 
-	function Plugin(select, options) {
-		this.$select = $(select);
-		this.$clicker = null;
-		this.$ul = null;
-		this.settings = $.extend({}, defaults, options);
+    function Plugin(select, options) {
+        this.$select = $(select);
+        this.$clicker = null;
+        this.$ul = null;
+        this.settings = $.extend({}, defaults, options);
 
-		create.apply(this);
-	}
+        create.apply(this);
+    }
 
-	function create() {
-		var scope = this;
-		var theme = this.settings.theme !== '' && this.settings.theme !== undefined ? ' ' + this.settings.theme : '';
+    function create() {
+        var scope = this;
+        var theme = this.settings.theme !== '' && this.settings.theme !== undefined ? ' ' + this.settings.theme : '';
 
-		this.$select.wrap('<div class="vDrop' + theme + '"></div>').parent().append('<a href="#" class="vClicker"><span></span><div class="vArrow"></div></a><ul></ul>');
+        this.$select.wrap('<div class="vDrop' + theme + '"></div>').parent().append('<a href="#" class="vClicker"><span></span><div class="vArrow"></div></a><ul></ul>');
 
-		this.$clicker = this.$select.siblings('.vClicker');
-		this.$ul = this.$select.siblings('ul');
+        this.$clicker = this.$select.siblings('.vClicker');
+        this.$ul = this.$select.siblings('ul');
 
-		this.$clicker.on('click', function (e) {
-			e.preventDefault();
-			e.stopPropagation();
+        this.$clicker.on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
-			if (selects.length === 0) {
-				$(document).on('click', function () {
-					scope.close();
-				});
-			}
+            if (selects.length === 0) {
+                $(document).on('click', function () {
+                    scope.close();
+                });
+            }
 
-			if (!scope.settings.allowMultiple) {
-				scope.close(null, scope.$select);
-			}
+            if (!scope.settings.allowMultiple) {
+                scope.close(null, scope.$select);
+            }
 
-			if (scope.$ul.find('li').length > 0) {
-				if ($(this).hasClass('open')) {
-					scope.close(scope.$select);
-				} else {
-					$(this).addClass('open');
+            if (scope.$ul.find('li').length > 0) {
+                if ($(this).hasClass('open')) {
+                    scope.close(scope.$select);
+                } else {
+                    $(this).addClass('open');
 
-					scope.$ul.stop(true).slideDown(scope.settings.transitionSpeed);
-				}
-			}
-		});
+                    scope.$ul.stop(true).slideDown(scope.settings.transitionSpeed);
+                }
+            }
+        });
 
-		if (selects.length === 0) {
-			$(document).on('click', function () {
-				scope.close();
-			});
-		}
+        if (selects.length === 0) {
+            $(document).on('click', function () {
+                scope.close();
+            });
+        }
 
-		selects.push(this.$select);
+        selects.push(this.$select);
 
-		this.update();
-	}
+        this.update();
+    }
 
-	function choose(index) {
-		if (index !== selectedOptionIndex.apply(this)) {
-			this.$select.find('option').removeAttr('selected').prop('selected', false);
-			this.$select.find('option').eq(index).attr('selected', 'selected').prop('selected', true);
+    function choose(index) {
+        if (index !== selectedOptionIndex.apply(this)) {
+            this.$select.find('option').removeAttr('selected').prop('selected', false);
+            this.$select.find('option').eq(index).attr('selected', 'selected').prop('selected', true);
 
-			updateUl.apply(this);
-		}
+            updateUl.apply(this);
+        }
 
-		this.close();
-	}
+        this.close();
+    }
 
-	function updateUl() {
-		this.$clicker.find('span').text(this.$select.find('option:selected').text());
+    function updateUl() {
+        this.$clicker.find('span').text(this.$select.find('option:selected').text());
 
-		this.$ul.find('a').removeClass('selected');
+        this.$ul.find('a').removeClass('selected');
 
-		this.$ul.find('a[data-index="' + selectedOptionIndex.apply(this) + '"]').addClass('selected');
-	}
+        this.$ul.find('a[data-index="' + selectedOptionIndex.apply(this) + '"]').addClass('selected');
+    }
 
-	function selectedOptionIndex() {
-		if (this.$select.find('optgroup').length > 0) {
-			var counter = 0;
-			var index = 0;
+    function selectedOptionIndex() {
+        if (this.$select.find('optgroup').length > 0) {
+            var counter = 0;
+            var index = 0;
 
-			$.each(this.$select.find('optgroup'), function () {
-				$.each($(this).find('option'), function () {
-					if ($(this).is(':selected')) {
-						index = counter;
-					}
+            $.each(this.$select.find('optgroup'), function () {
+                $.each($(this).find('option'), function () {
+                    if ($(this).is(':selected')) {
+                        index = counter;
+                    }
 
-					counter++;
-				});
-			});
+                    counter++;
+                });
+            });
 
-			return index;
-		} else {
-			return this.$select.find('option:selected').index();
-		}
-	}
+            return index;
+        } else {
+            return this.$select.find('option:selected').index();
+        }
+    }
 
-	$.extend(Plugin.prototype, {
-		update: function () {
-			var scope = this;
+    $.extend(Plugin.prototype, {
+        update: function () {
+            var scope = this;
 
-			this.$ul.find('a').unbind();
-			this.$ul.empty();
+            this.$ul.find('a').unbind();
+            this.$ul.empty();
 
             if (this.$select.find('optgroup').length > 0) {
                 var counter = 0;
 
-                                $.each(this.$select.find('optgroup'), function (a, optgroup) {
+                $.each(this.$select.find('optgroup'), function (a, optgroup) {
                     scope.$ul.append('<li class="optgroup"><span>' + $(optgroup).attr('label') + '</span></li>');
+
                     $.each($(this).find('option'), function (i, option) {
                         scope.$ul.append('<li><a href="#" data-value="' + $(option).val() + '" data-index="' + counter + '">' + $(option).text() + '</a></li>');
 
-                                                counter++;
+                        counter++;
                     });
                 });
             } else {
@@ -137,55 +138,55 @@
                 });
             }
 
-			if (this.$select.find('option[selected]').length === 0) {
-				this.$select.find('option').eq(0).attr('selected', 'selected');
-			}
+            if (this.$select.find('option[selected]').length === 0) {
+                this.$select.find('option').eq(0).attr('selected', 'selected');
+            }
 
-			if (this.$select.find('option').length !== 0) {
-				updateUl.apply(scope);
-			}
+            if (this.$select.find('option').length !== 0) {
+                updateUl.apply(scope);
+            }
 
-			this.$ul.find('a').unbind().on('click', function (e) {
-				e.preventDefault();
-				e.stopPropagation();
+            this.$ul.find('a').unbind().on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
 
-				choose.apply(scope, [parseInt($(this).attr('data-index'))]);
-			});
-		},
-		close: function ($select, $ignore) {
-			var scope = this;
+                choose.apply(scope, [parseInt($(this).attr('data-index'))]);
+            });
+        },
+        close: function ($select, $ignore) {
+            var scope = this;
 
-			if ($select instanceof $) {
-				if ($select.siblings('.vClicker').hasClass('open')) {
-					$select.siblings('.vClicker').removeClass('open');
+            if ($select instanceof $) {
+                if ($select.siblings('.vClicker').hasClass('open')) {
+                    $select.siblings('.vClicker').removeClass('open');
 
-					$select.siblings('ul').stop(true).slideUp(this.settings.transitionSpeed);
-				}
-			} else { 
-				$.each(selects, function () {
-					if ($ignore === undefined || this.attr('name') !== $ignore.attr('name')) {
-						scope.close(this);
-					}
-				});
-			}
-		}
-	});
+                    $select.siblings('ul').stop(true).slideUp(this.settings.transitionSpeed);
+                }
+            } else { 
+                $.each(selects, function () {
+                    if ($ignore === undefined || this.attr('name') !== $ignore.attr('name')) {
+                        scope.close(this);
+                    }
+                });
+            }
+        }
+    });
 
-	$.fn[pluginName] = function (options) {
-		return this.each(function () {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
-			} else {
-				switch (options) {
-					case 'update':
-						$(this).data('plugin_' + pluginName).update();
-						break;
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            } else {
+                switch (options) {
+                    case 'update':
+                        $(this).data('plugin_' + pluginName).update();
+                        break;
 
-					case 'close':
-						$(this).data('plugin_' + pluginName).close($(this));
-						break;
-				}
-			}
-		});
-	};
+                    case 'close':
+                        $(this).data('plugin_' + pluginName).close($(this));
+                        break;
+                }
+            }
+        });
+    };
 })(jQuery, window, document);
--- source/js/jquery.vdrop.js
+++ source/js/jquery.vdrop.js
@@ -1,131 +1,132 @@
 ;
 (function ($, window, document, undefined) {
-	'use strict';
+    'use strict';
 
-	var pluginName = 'vDrop';
-	var defaults = {
-		transitionSpeed: 150,
-		allowMultiple: true,
-		theme: ''
-	};
-	var selects = [];
+    var pluginName = 'vDrop';
+    var defaults = {
+        transitionSpeed: 150,
+        allowMultiple: true,
+        theme: ''
+    };
+    var selects = [];
 
-	function Plugin(select, options) {
-		this.$select = $(select);
-		this.$clicker = null;
-		this.$ul = null;
-		this.settings = $.extend({}, defaults, options);
+    function Plugin(select, options) {
+        this.$select = $(select);
+        this.$clicker = null;
+        this.$ul = null;
+        this.settings = $.extend({}, defaults, options);
 
-		create.apply(this);
-	}
+        create.apply(this);
+    }
 
-	function create() {
-		var scope = this;
-		var theme = this.settings.theme !== '' && this.settings.theme !== undefined ? ' ' + this.settings.theme : '';
+    function create() {
+        var scope = this;
+        var theme = this.settings.theme !== '' && this.settings.theme !== undefined ? ' ' + this.settings.theme : '';
 
-		this.$select.wrap('<div class="vDrop' + theme + '"></div>').parent().append('<a href="#" class="vClicker"><span></span><div class="vArrow"></div></a><ul></ul>');
+        this.$select.wrap('<div class="vDrop' + theme + '"></div>').parent().append('<a href="#" class="vClicker"><span></span><div class="vArrow"></div></a><ul></ul>');
 
-		this.$clicker = this.$select.siblings('.vClicker');
-		this.$ul = this.$select.siblings('ul');
+        this.$clicker = this.$select.siblings('.vClicker');
+        this.$ul = this.$select.siblings('ul');
 
-		this.$clicker.on('click', function (e) {
-			e.preventDefault();
-			e.stopPropagation();
+        this.$clicker.on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
-			if (selects.length === 0) {
-				$(document).on('click', function () {
-					scope.close();
-				});
-			}
+            if (selects.length === 0) {
+                $(document).on('click', function () {
+                    scope.close();
+                });
+            }
 
-			//allowMultiple=false then hide all dropdowns except clicked dropdown
-			if (!scope.settings.allowMultiple) {
-				scope.close(null, scope.$select);
-			}
+            //allowMultiple=false then hide all dropdowns except clicked dropdown
+            if (!scope.settings.allowMultiple) {
+                scope.close(null, scope.$select);
+            }
 
-			//only display list and change arrow states if there are options
-			if (scope.$ul.find('li').length > 0) {
-				if ($(this).hasClass('open')) {
-					scope.close(scope.$select);
-				} else {
-					$(this).addClass('open');
+            //only display list and change arrow states if there are options
+            if (scope.$ul.find('li').length > 0) {
+                if ($(this).hasClass('open')) {
+                    scope.close(scope.$select);
+                } else {
+                    $(this).addClass('open');
 
-					scope.$ul.stop(true).slideDown(scope.settings.transitionSpeed);
-				}
-			}
-		});
+                    scope.$ul.stop(true).slideDown(scope.settings.transitionSpeed);
+                }
+            }
+        });
 
-		if (selects.length === 0) {
-			$(document).on('click', function () {
-				scope.close();
-			});
-		}
+        if (selects.length === 0) {
+            $(document).on('click', function () {
+                scope.close();
+            });
+        }
 
-		//store all selects in an array for allowMultiple=false conditions
-		selects.push(this.$select);
+        //store all selects in an array for allowMultiple=false conditions
+        selects.push(this.$select);
 
-		this.update();
-	}
+        this.update();
+    }
 
-	function choose(index) {
-		if (index !== selectedOptionIndex.apply(this)) {
-			this.$select.find('option').removeAttr('selected').prop('selected', false);
-			this.$select.find('option').eq(index).attr('selected', 'selected').prop('selected', true);
+    function choose(index) {
+        if (index !== selectedOptionIndex.apply(this)) {
+            this.$select.find('option').removeAttr('selected').prop('selected', false);
+            this.$select.find('option').eq(index).attr('selected', 'selected').prop('selected', true);
 
-			updateUl.apply(this);
-		}
+            updateUl.apply(this);
+        }
 
-		this.close();
-	}
+        this.close();
+    }
 
-	function updateUl() {
-		this.$clicker.find('span').text(this.$select.find('option:selected').text());
+    function updateUl() {
+        this.$clicker.find('span').text(this.$select.find('option:selected').text());
 
-		//remove selected class from all anchors
-		this.$ul.find('a').removeClass('selected');
+        //remove selected class from all anchors
+        this.$ul.find('a').removeClass('selected');
 
-		//add selected class based on selected option index
-		this.$ul.find('a[data-index="' + selectedOptionIndex.apply(this) + '"]').addClass('selected');
-	}
+        //add selected class based on selected option index
+        this.$ul.find('a[data-index="' + selectedOptionIndex.apply(this) + '"]').addClass('selected');
+    }
 
-	function selectedOptionIndex() {
-		if (this.$select.find('optgroup').length > 0) {
-			var counter = 0;
-			var index = 0;
+    function selectedOptionIndex() {
+        if (this.$select.find('optgroup').length > 0) {
+            var counter = 0;
+            var index = 0;
 
-			$.each(this.$select.find('optgroup'), function () {
-				$.each($(this).find('option'), function () {
-					if ($(this).is(':selected')) {
-						index = counter;
-					}
+            $.each(this.$select.find('optgroup'), function () {
+                $.each($(this).find('option'), function () {
+                    if ($(this).is(':selected')) {
+                        index = counter;
+                    }
 
-					counter++;
-				});
-			});
+                    counter++;
+                });
+            });
 
-			return index;
-		} else {
-			return this.$select.find('option:selected').index();
-		}
-	}
+            return index;
+        } else {
+            return this.$select.find('option:selected').index();
+        }
+    }
 
-	$.extend(Plugin.prototype, {
-		update: function () {
-			var scope = this;
+    $.extend(Plugin.prototype, {
+        update: function () {
+            var scope = this;
 
-			this.$ul.find('a').unbind();
-			this.$ul.empty();
+            this.$ul.find('a').unbind();
+            this.$ul.empty();
 
-			//#TODO: check for grouping and wrap insert LI with no anchor
-			//generate LIs for each option
+            //#TODO: check for grouping and wrap insert LI with no anchor
+            //generate LIs for each option
             if (this.$select.find('optgroup').length > 0) {
                 var counter = 0;
-                
+
                 $.each(this.$select.find('optgroup'), function (a, optgroup) {
                     scope.$ul.append('<li class="optgroup"><span>' + $(optgroup).attr('label') + '</span></li>');
+
                     $.each($(this).find('option'), function (i, option) {
                         scope.$ul.append('<li><a href="#" data-value="' + $(option).val() + '" data-index="' + counter + '">' + $(option).text() + '</a></li>');
-                        
+
                         counter++;
                     });
                 });
@@ -135,58 +136,58 @@
                 });
             }
 
-			//no option is selected set it to the first
-			if (this.$select.find('option[selected]').length === 0) {
-				this.$select.find('option').eq(0).attr('selected', 'selected');
-			}
+            //no option is selected set it to the first
+            if (this.$select.find('option[selected]').length === 0) {
+                this.$select.find('option').eq(0).attr('selected', 'selected');
+            }
 
-			if (this.$select.find('option').length !== 0) {
-				updateUl.apply(scope);
-			}
+            if (this.$select.find('option').length !== 0) {
+                updateUl.apply(scope);
+            }
 
-			this.$ul.find('a').unbind().on('click', function (e) {
-				e.preventDefault();
-				e.stopPropagation();
+            this.$ul.find('a').unbind().on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
 
-				choose.apply(scope, [parseInt($(this).attr('data-index'))]);
-			});
-		},
-		close: function ($select, $ignore) {
-			var scope = this;
+                choose.apply(scope, [parseInt($(this).attr('data-index'))]);
+            });
+        },
+        close: function ($select, $ignore) {
+            var scope = this;
 
-			if ($select instanceof $) {
-				//only close dropdown if it's open
-				if ($select.siblings('.vClicker').hasClass('open')) {
-					$select.siblings('.vClicker').removeClass('open');
+            if ($select instanceof $) {
+                //only close dropdown if it's open
+                if ($select.siblings('.vClicker').hasClass('open')) {
+                    $select.siblings('.vClicker').removeClass('open');
 
-					$select.siblings('ul').stop(true).slideUp(this.settings.transitionSpeed);
-				}
-			} else { //no select was specified then hide all. Re-call hide method with select as parameter
-				$.each(selects, function () {
-					//don't hide clicked select
-					if ($ignore === undefined || this.attr('name') !== $ignore.attr('name')) {
-						scope.close(this);
-					}
-				});
-			}
-		}
-	});
+                    $select.siblings('ul').stop(true).slideUp(this.settings.transitionSpeed);
+                }
+            } else { //no select was specified then hide all. Re-call hide method with select as parameter
+                $.each(selects, function () {
+                    //don't hide clicked select
+                    if ($ignore === undefined || this.attr('name') !== $ignore.attr('name')) {
+                        scope.close(this);
+                    }
+                });
+            }
+        }
+    });
 
-	$.fn[pluginName] = function (options) {
-		return this.each(function () {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
-			} else {
-				switch (options) {
-					case 'update':
-						$(this).data('plugin_' + pluginName).update();
-						break;
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            } else {
+                switch (options) {
+                    case 'update':
+                        $(this).data('plugin_' + pluginName).update();
+                        break;
 
-					case 'close':
-						$(this).data('plugin_' + pluginName).close($(this));
-						break;
-				}
-			}
-		});
-	};
+                    case 'close':
+                        $(this).data('plugin_' + pluginName).close($(this));
+                        break;
+                }
+            }
+        });
+    };
 })(jQuery, window, document);
